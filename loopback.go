85136ab3ea603305dea582d45042fa7e4ea7577b
-Aln0 <2629184561@qq.com>
2023-04-20 17:14:48 +0800

    pathfs test case for directory.

--- loopback.go
+++ loopback.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"sync"
 	"syscall"
 	"time"
 )
@@ -29,9 +28,8 @@ func NewLoopbackFileSystem(root string) FileSystem {
 		panic(err)
 	}
 
-
 	return &loopbackFileSystem{
-		root: root
+		root: root,
 	}
 }
 
@@ -40,10 +38,9 @@ func (fs *loopbackFileSystem) absPath(relPath string) string {
 }
 
 func (fs *loopbackFileSystem) GetAttr(ctx *Context, path string, uFh uint32, out *fuse.Attr) fuse.Status {
-
 	var err error = nil
 	st := syscall.Stat_t{}
-	if uFh != 0 {
+	if uFh > 3 {
 		err = syscall.Fstat(int(uFh), &st)
 	} else {
 		absPath := fs.absPath(path)
@@ -109,27 +106,94 @@ func (fs *loopbackFileSystem) Create(ctx *Context, path string, flags uint32, mo
 
 func (fs *loopbackFileSystem) Open(ctx *Context, path string, flags uint32) (uFh uint32, keepCache, forceDIO bool, code fuse.Status) {
 	fd, err := syscall.Open(fs.absPath(path), int(flags), 0)
+	forceDIO = flags&syscall.O_DIRECT != 0
 	if err != nil {
-		return 0, false, false, fuse.ToStatus(err)
+		code = fuse.ToStatus(err)
+		return
 	}
 	uFh = uint32(fd)
 	return
 }
 
-func (fs *loopbackFileSystem) Chmod(ctx *Context, path string, uFh uint32, mode uint32) fuse.Status {
+func (fs *loopbackFileSystem) Read(ctx *Context, path string, uFh uint32, dest []byte, off uint64) (result fuse.ReadResult, code fuse.Status) {
 	var err error
-	if uFh != 0 {
-		err = syscall.Chmod(fs.absPath(path), mode)
+	if uFh > 3 {
+		_, err = syscall.Pread(int(uFh), dest, int64(off))
 	} else {
-		err = syscall.Fchmod(int(uFh), mode)
+		f, err := os.Open(path)
+		defer f.Close()
+		if err != nil {
+			return nil, fuse.ToStatus(err)
+		}
+		_, err = f.ReadAt(dest, int64(off))
 	}
 
+	if err != nil && err != io.EOF {
+		return nil, fuse.ToStatus(err)
+	}
+
+	return fuse.ReadResultData(dest), fuse.OK
+}
+
+func (fs *loopbackFileSystem) Write(ctx *Context, path string, uFh uint32, data []byte, off uint64) (written uint32, code fuse.Status) {
+	var err error
+	var n int
+	if uFh > 3 {
+		n, err = syscall.Pwrite(int(uFh), data, int64(off))
+	} else {
+		f, e := os.Open(fs.absPath(path))
+		defer f.Close()
+		if e != nil {
+			return 0, fuse.ToStatus(e)
+		}
+		n, err = f.WriteAt(data, int64(off))
+	}
+
+	return uint32(n), fuse.ToStatus(err)
+}
+
+func (fs *loopbackFileSystem) Fallocate(ctx *Context, path string, uFh uint32, off uint64, size uint64, mode uint32) fuse.Status {
+	var err error
+	if uFh > 3 {
+		err = syscall.Fallocate(int(uFh), mode, int64(off), int64(size))
+	} else {
+		fd, e := syscall.Open(fs.absPath(path), 0, 0)
+		defer syscall.Close(fd)
+		if e != nil {
+			return fuse.ToStatus(e)
+		}
+		err = syscall.Fallocate(fd, mode, int64(off), int64(size))
+	}
+	return fuse.ToStatus(err)
+}
+
+func (fs *loopbackFileSystem) Fsync(ctx *Context, path string, uFh uint32, flags uint32) fuse.Status {
+	if uFh > 3 {
+		return fuse.ToStatus(syscall.Fsync(int(uFh)))
+	} else {
+		return fuse.OK
+	}
+}
+
+func (fs *loopbackFileSystem) Release(ctx *Context, path string, uFh uint32) {
+	if uFh > 3 {
+		fuse.ToStatus(syscall.Close(int(uFh)))
+	}
+}
+
+func (fs *loopbackFileSystem) Chmod(ctx *Context, path string, uFh uint32, mode uint32) fuse.Status {
+	var err error
+	if uFh > 3 {
+		err = syscall.Fchmod(int(uFh), mode)
+	} else {
+		err = syscall.Chmod(fs.absPath(path), mode)
+	}
 	return fuse.ToStatus(err)
 }
 
 func (fs *loopbackFileSystem) Chown(ctx *Context, path string, uFh uint32, uid uint32, gid uint32) fuse.Status {
 	var err error
-	if uFh != 0 {
+	if uFh > 3 {
 		err = syscall.Fchown(int(uFh), int(uid), int(gid))
 	} else {
 		err = syscall.Chown(path, int(uid), int(gid))
@@ -139,23 +203,26 @@ func (fs *loopbackFileSystem) Chown(ctx *Context, path string, uFh uint32, uid u
 
 func (fs *loopbackFileSystem) Truncate(ctx *Context, path string, uFh uint32, size uint64) fuse.Status {
 	var err error
-	if uFh != 0 {
+	if uFh > 3 {
 		err = syscall.Ftruncate(int(uFh), int64(size))
 	} else {
-		err = syscall.Truncate(path, int64(size))
+		err = os.Truncate(path, int64(size))
 	}
 	return fuse.ToStatus(err)
 }
 
 func (fs *loopbackFileSystem) Utimens(ctx *Context, path string, uFh uint32, atime *time.Time, mtime *time.Time) fuse.Status {
 	var err error
-	if uFh != 0 {
-		atime.Second()
-		err = syscall.Futimes(int(uFh), []syscall.Timeval{
-			{Sec: atime.},
-			{},
-		})
+	timevals := []syscall.Timeval{
+		{Sec: atime.Unix(), Usec: int64(atime.Nanosecond())},
+		{Sec: mtime.Unix(), Usec: int64(mtime.Nanosecond())},
 	}
+	if uFh > 3 {
+		err = syscall.Futimes(int(uFh), timevals)
+	} else {
+		err = syscall.Utimes(path, timevals)
+	}
+	return fuse.ToStatus(err)
 }
 
 func (fs *loopbackFileSystem) Lsdir(ctx *Context, path string) (stream []fuse.DirEntry, code fuse.Status) {
@@ -193,13 +260,13 @@ func (fs *loopbackFileSystem) Lsdir(ctx *Context, path string) (stream []fuse.Di
 	return stream, fuse.OK
 }
 
-func (fs *loopbackFileSystem) StatFs(name string) *fuse.StatfsOut {
+func (fs *loopbackFileSystem) StatFs(ctx *Context, path string, out *fuse.StatfsOut) fuse.Status {
 	s := syscall.Statfs_t{}
-	err := syscall.Statfs(fs.absPath(name), &s)
-	if err == nil {
-		out := &fuse.StatfsOut{}
-		out.FromStatfsT(&s)
-		return out
+	err := syscall.Statfs(fs.absPath(path), &s)
+	if err != nil {
+		return fuse.ToStatus(err)
 	}
-	return nil
+	out = &fuse.StatfsOut{}
+	out.FromStatfsT(&s)
+	return fuse.OK
 }
